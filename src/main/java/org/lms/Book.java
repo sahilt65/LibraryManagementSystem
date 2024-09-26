454c772c6e18ba80cbed070b74e16a4d9c19e3a9
sahilt65 <sahiltpict@gmail.com>
2024-09-27 02:19:53 +0530

    Add test for adding book

--- src/main/java/org/lms/Book.java
+++ src/main/java/org/lms/Book.java
@@ -5,12 +5,14 @@ public class Book {
     private String title;
     private String author;
     private int year;
+    private boolean isBorrowed;
 
     public Book(String isbn, String title, String author, int year) {
         this.isbn = isbn;
         this.title = title;
         this.author = author;
         this.year = year;
+        this.isBorrowed = false;
     }
 
     public String getIsbn() {
@@ -27,6 +29,10 @@ public class Book {
 
     public int getYear() {
         return year;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
     }
 }
 
